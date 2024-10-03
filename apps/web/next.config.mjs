87a4dfd63bd142c752521cfffab4b1b3287da915
Harsh Sahu <yesh8harsh+github@gmail.com>
2024-10-03 08:27:16 +0530

    feat: Nextjs 15, react 19 - babel compiler
    Experimental nextjs config

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    reactCompiler: {
+      compilationMode: 'annotation',
+    },
+    staleTimes: {
+      dynamic: 30,
+    },
+  },
+  compiler: {
+    styledComponents: true,
+  }
+};
 
 export default nextConfig;
