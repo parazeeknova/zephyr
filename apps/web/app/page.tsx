8bf9bdfe828b3d7f8147eb976f1981a1922bc314
Harsh Sahu <yesh8harsh+github@gmail.com>
2024-10-05 19:46:50 +0530

    fix[shadcnui]: Structure change, custom brand colors
    Initialized ui components shad

--- apps/web/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <h1 className="mt-5 flex justify-center text-center font-semibold">
-      Welcome to Zephyr
-    </h1>
-  );
-}
---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="mt-5 flex justify-center text-center font-semibold">
+        Welcome to Zephyr
+      </h1>
+    </>
+  );
+}
