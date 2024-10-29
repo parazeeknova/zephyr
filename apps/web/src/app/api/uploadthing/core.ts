f81b2e14a2e6fcaa2317be271e857e942c4cb3a0
Harsh Sahu <yesh8harsh+github@gmail.com>
2024-10-29 23:35:04 +0530

    fix[G-AVATAR]: Minor fix for changing profile for Google Oauth users
    Google logged in people can now change there profile picture :)

--- apps/web/src/app/api/uploadthing/core.ts
+++ apps/web/src/app/api/uploadthing/core.ts
@@ -13,20 +13,25 @@ export const fileRouter = {
   })
     .middleware(async () => {
       const { user } = await validateRequest();
-
       if (!user) throw new UploadThingError("Unauthorized");
-
       return { user };
     })
     .onUploadComplete(async ({ metadata, file }) => {
       const oldAvatarUrl = metadata.user.avatarUrl;
 
-      if (oldAvatarUrl) {
-        const key = oldAvatarUrl.split(
-          `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`
-        )[1];
-
-        await new UTApi().deleteFiles(key);
+      if (
+        oldAvatarUrl?.includes(
+          `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}`
+        )
+      ) {
+        try {
+          const key = oldAvatarUrl.split(
+            `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`
+          )[1];
+          await new UTApi().deleteFiles(key);
+        } catch (error) {
+          console.error("Failed to delete old avatar:", error);
+        }
       }
 
       const newAvatarUrl = file.url.replace(
