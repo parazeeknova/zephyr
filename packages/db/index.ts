055af816956155650bc8074b9d00d37e529a8a28
Harsh Sahu <yesh8harsh+github@gmail.com>
2024-11-01 17:29:29 +0530

    fix[REDIS]: Fixed view count bug
    Will still update every day at 1 am

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,5 +1,4 @@
 export * from "./cache/search-cache";
-export * from "./cache/view-cache";
 export * from "./src/client";
 export { default as prisma } from "./src/prisma";
 export * from "./src/redis";
