99207316ccf2e48522579737aef402b671ac62ff
Harsh Sahu <yesh8harsh+github@gmail.com>
2024-11-16 20:10:18 +0530

    chore[HBS]: Added ui templates for components, hooks and more..
    Will refactor these soon in upcoming future

--- packages/ui/turbo/generators/config.ts
+++ packages/ui/turbo/generators/config.ts
@@ -1,29 +1,133 @@
 import type { PlopTypes } from "@turbo/gen";
 
-// Learn more about Turborepo Generators at https://turbo.build/repo/docs/core-concepts/monorepos/code-generation
-
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
-  // A simple generator to add a new React component to the internal UI library
-  plop.setGenerator("react-component", {
-    description: "Adds a new react component",
+  // Server Action Generator
+  plop.setGenerator("action", {
+    description: "Create a new server action",
     prompts: [
       {
         type: "input",
         name: "name",
-        message: "What is the name of the component?"
+        message: "What is the name of your action?"
+      },
+      {
+        type: "input",
+        name: "path",
+        message: "Where should this action be created? (e.g., app/actions)"
       }
     ],
     actions: [
       {
         type: "add",
-        path: "src/{{kebabCase name}}.tsx",
-        templateFile: "templates/component.hbs"
+        path: "{{path}}/{{camelCase name}}.ts",
+        templateFile: "templates/server-action.hbs"
+      }
+    ]
+  });
+
+  // API Route Generator
+  plop.setGenerator("route", {
+    description: "Create a new API route",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of your route?"
       },
       {
-        type: "append",
-        path: "package.json",
-        pattern: /"exports": {(?<insertion>)/g,
-        template: '    "./{{kebabCase name}}": "./src/{{kebabCase name}}.tsx",'
+        type: "input",
+        name: "path",
+        message: "Where should this route be created? (e.g., app/api/users)"
+      }
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "{{path}}/route.ts",
+        templateFile: "templates/route.hbs"
+      }
+    ]
+  });
+
+  // Page Generator
+  plop.setGenerator("page", {
+    description: "Create a new page",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of your page?"
+      },
+      {
+        type: "input",
+        name: "path",
+        message: "Where should this page be created? (e.g., app/users)"
+      },
+      {
+        type: "confirm",
+        name: "isClient",
+        message: "Is this a client component?",
+        default: false
+      }
+    ],
+    actions: (data) => {
+      const actions = [];
+      const template = data?.isClient ? "client-page.hbs" : "page.hbs";
+
+      actions.push({
+        type: "add",
+        path: "{{path}}/page.tsx",
+        templateFile: `templates/${template}`
+      });
+
+      return actions;
+    }
+  });
+
+  // Mutation Generator
+  plop.setGenerator("mutation", {
+    description: "Create a new mutation",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of your mutation?"
+      },
+      {
+        type: "input",
+        name: "path",
+        message: "Where should this mutation be created? (e.g., mutations)"
+      }
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "{{path}}/{{camelCase name}}.ts",
+        templateFile: "templates/mutation.hbs"
+      }
+    ]
+  });
+
+  // Hook Generator
+  plop.setGenerator("hook", {
+    description: "Create a new React hook",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of your hook? (without 'use' prefix)"
+      },
+      {
+        type: "input",
+        name: "path",
+        message: "Where should this hook be created? (e.g., hooks)"
+      }
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "{{path}}/use{{pascalCase name}}.tsx",
+        templateFile: "templates/hook.hbs"
       }
     ]
   });
