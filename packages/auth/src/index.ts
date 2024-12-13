1002c7f3e0ffcfdf5ac308a52b7f4a106540cc41
Harsh Sahu <yesh8harsh+github@gmail.com>
2024-12-13 15:55:23 +0530

    chore[REFACTOR]: Proper auth package dir struct
    FIxing messy just working code

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,1 +1,22 @@
 export * from "./stream/services";
+
+// Core exports
+export { lucia, validateRequest } from "./core/lucia";
+
+// OAuth providers
+export { google, github, discord, twitter } from "./providers/oauth";
+
+// Validation exports
+export { DISPOSABLE_EMAIL_DOMAINS } from "./validation/constants";
+export {
+  signUpSchema,
+  loginSchema,
+  createPostSchema,
+  updateUserProfileSchema,
+  type SignUpValues,
+  type LoginValues,
+  type UpdateUserProfileValues
+} from "./validation/schemas";
+
+// Email service exports (from previous restructuring)
+export { sendVerificationEmail, sendPasswordResetEmail } from "./email/service";
